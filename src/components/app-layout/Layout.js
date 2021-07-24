88d35806ec666593231051cb201ee1861bc5f553
chiefphils <macphilip6@gmail.com>
2021-07-24 20:48:06 +0530

    added layout, variables, mixins and some assets

---
+++ src/components/app-layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Layout.scss'
+
+function Layout(props) {
+  return (
+    <div className='layout'>
+      { props.children }
+    </div>
+  )
+}
+
+export default Layout
