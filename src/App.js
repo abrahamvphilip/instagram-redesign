2a77e98f5943a84c944c9397e6b382dd6c05a351
chiefphils <macphilip6@gmail.com>
2021-07-26 01:53:39 +0530

    added navbarTop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 
 import Layout from "./components/app-layout/Layout";
+import NavbarTop from "./components/navbar-top/NavbarTop";
 import Story from "./components/story-section/Story";
 // import Login from "./components/Login/Login";
 
 function App() {
 	return (
 		<Layout>
+			<NavbarTop />
 			<Story />
 			{/* <Login /> */}
 		</Layout>
