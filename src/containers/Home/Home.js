016af4d9a2bf6398115ccde23b2f20845cbf591f
Parag Katoch <paragkatoch5@gmail.com>
2021-07-26 23:46:53 +0530

    Navbar skeleton

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Post from "../../components/feed-post/Post";
-import NavbarTop from "../../components/navbar-top/NavbarTop";
 import Story from "../../components/story-section/Story";
+import Navbar from "../Navbar/Navbar";
 
 export default function Home() {
 	return (
 		<main className="Home">
-			<NavbarTop />
+			<Navbar />
 			<Story />
 			<Post />
 		</main>
