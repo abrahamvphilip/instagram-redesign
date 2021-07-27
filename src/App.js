28547c16e18ce1414b7b30cd3e019624f742175b
Parag Katoch <paragkatoch5@gmail.com>
2021-07-27 19:24:39 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.scss";
 
 import Layout from "./containers/app-layout/Layout";
-import Home from "./containers/Home/Home";
+import Routes from "./containers/Routes/Route";
 // import Login from "./components/Login/Login";
 
 function App() {
 	return (
 		<Layout>
-			<Home />
+			<Routes />
 		</Layout>
 	);
 }
